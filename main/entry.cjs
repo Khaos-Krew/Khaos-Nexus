c3c70c7e1d1921b73668efecdc0ce4c74c4d25d1
Khaos_Kirito <dasak93@gmail.com>
2026-07-23 13:08:48 -0500

    Load the Mobile Companion foundation

--- main/entry.cjs
+++ main/entry.cjs
@@ -3,5 +3,6 @@
 require('./brand-update-extension.cjs').install();
 require('./palworld-main-extension.cjs').install();
 require('./discord-studio-extension.cjs').install();
+require('./mobile-gateway-extension.cjs').install();
 require('./module-foundation-extension.cjs').install();
 require('./main.cjs');
