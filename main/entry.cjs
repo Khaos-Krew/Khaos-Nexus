225750a0ec6f9a45ac8c2b922b8b92798bc6203d
Khaos_Kirito <dasak93@gmail.com>
2026-08-01 17:12:41 -0500

    Wire D&D draft preservation before access policy

--- main/entry.cjs
+++ main/entry.cjs
@@ -48,6 +48,7 @@ if (diagnosticsMode) {
     require('./dnd-npc-tool-extension.cjs').install();
     require('./dnd-encounter-panels-extension.cjs').install();
     require('./dnd-encounter-panels-stability-extension.cjs').install();
+    require('./dnd-draft-preservation-extension.cjs').install();
     require('./dnd-access-policy-extension.cjs').install();
     require('./dnd-authorization-summary-extension.cjs').install();
 
