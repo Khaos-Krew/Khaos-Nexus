289bb934d5b684dd93269190bbd12773b976469d
Khaos_Kirito <dasak93@gmail.com>
2026-08-22 18:17:19 -0500

    rebuild: add Railway Sentinal composite entrypoint

---
+++ src/railway/sentinal-service.cjs
@@ -0,0 +1,9 @@
+'use strict';
+
+process.env.NEXUS_BACKEND_HOST ||= '127.0.0.1';
+process.env.NEXUS_BACKEND_PORT ||= '3210';
+process.env.NEXUS_BACKEND_URL ||= `http://${process.env.NEXUS_BACKEND_HOST}:${process.env.NEXUS_BACKEND_PORT}`;
+
+console.log('[Nexus Sentinal] starting Railway composite runtime');
+require('../backend/server.cjs');
+require('../sentinel/bot.cjs');
