db6451fd127321d20107d5876b957a8ad81a78b8
Khaos_Kirito <dasak93@gmail.com>
2026-08-22 19:44:56 -0500

    feat(division2): register native community-data provider

--- src/backend/providers/native-providers.cjs
+++ src/backend/providers/native-providers.cjs
@@ -1,6 +1,7 @@
 'use strict';
 
 const { WarframeProvider } = require('./warframe-provider.cjs');
+const { Division2Provider, DIVISION2_ACTIONS } = require('./division2-provider.cjs');
 
 const WARFRAME_ACTIONS = Object.freeze([
   'alerts', 'fissures', 'sortie', 'arbitration', 'nightwave', 'market', 'builds'
@@ -17,7 +18,12 @@ function nativeProvidersFromConfig(config = {}, options = {}) {
     warframe.supportedActions = [...WARFRAME_ACTIONS];
     providers.warframe = warframe;
   }
+  if (config.modules?.division2?.enabled !== false) {
+    const division2 = new Division2Provider({ fetchImpl: options.fetchImpl });
+    division2.supportedActions = [...DIVISION2_ACTIONS];
+    providers.division2 = division2;
+  }
   return providers;
 }
 
-module.exports = { nativeProvidersFromConfig, WARFRAME_ACTIONS };
+module.exports = { nativeProvidersFromConfig, WARFRAME_ACTIONS, DIVISION2_ACTIONS };
