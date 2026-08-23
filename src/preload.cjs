e0439aafba9c7b11cc06965f5a5d69e6a399b20a
Khaos_Kirito <dasak93@gmail.com>
2026-08-23 14:08:01 -0500

    feat: securely pair desktop to hosted Sentinal

--- src/preload.cjs
+++ src/preload.cjs
@@ -1,6 +1,7 @@
 'use strict';
 
 const { contextBridge, ipcRenderer } = require('electron');
+const { pairRequest } = require('./desktop/sentinal-pairing.cjs');
 
 contextBridge.exposeInMainWorld('nexusAdmin', {
   state: () => ipcRenderer.invoke('nexus:state'),
@@ -16,6 +17,15 @@ contextBridge.exposeInMainWorld('nexusAdmin', {
   removeAccount: (accountId) => ipcRenderer.invoke('nexus:remove-account', accountId),
   validateProviders: (moduleId) => ipcRenderer.invoke('nexus:validate-providers', moduleId || ''),
 
+  sentinalPair: async (url, code) => {
+    const paired = await pairRequest(url, code);
+    const state = await ipcRenderer.invoke('nexus:state');
+    const next = JSON.parse(JSON.stringify(state.settings || {}));
+    next.discord ||= {};
+    next.discord.sentinalAdminUrl = paired.baseUrl;
+    await ipcRenderer.invoke('nexus:set-secret', 'NEXUS_SENTINAL_ADMIN_TOKEN', paired.token);
+    return ipcRenderer.invoke('nexus:save-settings', next);
+  },
   sentinalStatus: () => ipcRenderer.invoke('nexus:sentinal-status'),
   sentinalPermissions: () => ipcRenderer.invoke('nexus:sentinal-permissions'),
   sentinalCommands: () => ipcRenderer.invoke('nexus:sentinal-commands'),
