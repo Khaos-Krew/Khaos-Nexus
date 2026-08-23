5142ad195df895f9ceb00b40e8eb2f7af7d44f06
Khaos_Kirito <dasak93@gmail.com>
2026-08-23 14:06:35 -0500

    feat: install hosted Sentinal pairing extension

--- src/sentinel/entry.cjs
+++ src/sentinel/entry.cjs
@@ -2,7 +2,9 @@
 
 const { installPokemonGoExtension } = require('./pokemon-go-extension.cjs');
 const { installEventFeedExtension } = require('./event-feed-extension.cjs');
+const { installAdminPairingExtension } = require('./admin-pairing-extension.cjs');
 
 installPokemonGoExtension();
 installEventFeedExtension();
+installAdminPairingExtension();
 require('./bot.cjs');
