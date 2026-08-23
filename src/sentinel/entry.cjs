8ceafd7e3a46f478876c28f2c6e8b821a928a535
Khaos_Kirito <dasak93@gmail.com>
2026-08-23 14:36:50 -0500

    merge: preserve current Sentinal extensions with hosted pairing

--- src/sentinel/entry.cjs
+++ src/sentinel/entry.cjs
@@ -1,9 +1,11 @@
 'use strict';
 
+const { installRoleMenuExtension } = require('./role-menu-extension.cjs');
 const { installPokemonGoExtension } = require('./pokemon-go-extension.cjs');
 const { installEventFeedExtension } = require('./event-feed-extension.cjs');
 const { installAdminPairingExtension } = require('./admin-pairing-extension.cjs');
 
+installRoleMenuExtension();
 installPokemonGoExtension();
 installEventFeedExtension();
 installAdminPairingExtension();
