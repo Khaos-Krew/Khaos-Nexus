062d5802e96c11bda4561114ffdc812733546f29
Khaos_Kirito <dasak93@gmail.com>
2026-08-23 00:36:57 -0500

    desktop: expose admin control IPC bridge

--- src/preload.cjs
+++ src/preload.cjs
@@ -5,5 +5,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('nexusAdmin', {
   state: () => ipcRenderer.invoke('nexus:state'),
   diagnostics: () => ipcRenderer.invoke('nexus:diagnostics'),
+  exportDiagnostics: () => ipcRenderer.invoke('nexus:export-diagnostics'),
+  restartBackend: () => ipcRenderer.invoke('nexus:restart-backend'),
+  saveSettings: (settings) => ipcRenderer.invoke('nexus:save-settings', settings),
+  setSecret: (name, value) => ipcRenderer.invoke('nexus:set-secret', name, value),
+  clearSecret: (name) => ipcRenderer.invoke('nexus:clear-secret', name),
+  openDataFolder: () => ipcRenderer.invoke('nexus:open-data-folder'),
+  chooseThora: () => ipcRenderer.invoke('nexus:choose-thora'),
   launchThora: () => ipcRenderer.invoke('nexus:thora-launch')
 });
