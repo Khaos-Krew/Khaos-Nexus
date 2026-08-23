a4c6fb1d8b9d25a71e5099968f7442e8274c7997
Khaos_Kirito <dasak93@gmail.com>
2026-08-23 12:47:13 -0500

    feat(sentinal): load dynamic access role extension

--- src/sentinel/entry.cjs
+++ src/sentinel/entry.cjs
@@ -1,6 +1,8 @@
 'use strict';
 
+const { installRoleMenuExtension } = require('./role-menu-extension.cjs');
 const { installPokemonGoExtension } = require('./pokemon-go-extension.cjs');
 
+installRoleMenuExtension();
 installPokemonGoExtension();
 require('./bot.cjs');
