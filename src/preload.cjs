75d6460d725ebbf7c0d230d035ca23f0cb43335f
Khaos_Kirito <dasak93@gmail.com>
2026-08-23 10:58:28 -0500

    feat(accounts): expose account-linking desktop API

--- src/preload.cjs
+++ src/preload.cjs
@@ -10,6 +10,9 @@ contextBridge.exposeInMainWorld('nexusAdmin', {
   saveSettings: (settings) => ipcRenderer.invoke('nexus:save-settings', settings),
   setSecret: (name, value) => ipcRenderer.invoke('nexus:set-secret', name, value),
   clearSecret: (name) => ipcRenderer.invoke('nexus:clear-secret', name),
+  createAccountLinkCode: (role) => ipcRenderer.invoke('nexus:create-account-link-code', role),
+  linkDiscordOAuth: (role) => ipcRenderer.invoke('nexus:link-discord-oauth', role),
+  removeAccount: (accountId) => ipcRenderer.invoke('nexus:remove-account', accountId),
   openDataFolder: () => ipcRenderer.invoke('nexus:open-data-folder'),
   chooseThora: () => ipcRenderer.invoke('nexus:choose-thora'),
   launchThora: () => ipcRenderer.invoke('nexus:thora-launch')
