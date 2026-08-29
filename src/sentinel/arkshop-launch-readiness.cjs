39f35fdab75c7759e5520cfdb3a30fa197666cf6
Khaos_Kirito <dasak93@gmail.com>
2026-08-29 14:41:17 -0500

    ARK: require in-shop cache buttons for launch readiness

--- src/sentinel/arkshop-launch-readiness.cjs
+++ src/sentinel/arkshop-launch-readiness.cjs
@@ -4,6 +4,7 @@ const { KIT_PRICES } = require('./arkshop-nexus-launch-v3-kits-startup.cjs');
 const { REBUNDLED_BUYS, BASIC_SELLS } = require('./arkshop-nexus-launch-v7-basic-sell-startup.cjs');
 const { BOSS_TROPHY_SELLS } = require('./arkshop-nexus-launch-v8-boss-sell-startup.cjs');
 const { APEX_TRIBUTE_SELLS } = require('./arkshop-nexus-launch-v9-apex-tribute-sell-startup.cjs');
+const { CACHE_IDS, cacheShopId } = require('./arkshop-nexus-launch-v11-inshop-caches-startup.cjs');
 
 const REQUIRED_DINO_BALL_ITEMS = Object.freeze(['dinoballs5', 'dinoballs25', 'dinoballs100']);
 
@@ -18,6 +19,7 @@ function launchReadiness(profile) {
   for (const [id, price] of Object.entries(KIT_PRICES)) if (Number(kits[id]?.Price) !== Number(price)) missing.push(`kit:${id}`);
   for (const id of REQUIRED_DINO_BALL_ITEMS) if (!shop[id]) missing.push(`shop:${id}`);
   for (const id of Object.keys(REBUNDLED_BUYS)) if (!shop[id]) missing.push(`resource-buy:${id}`);
+  for (const cacheId of CACHE_IDS) if (!shop[cacheShopId(cacheId)]) missing.push(`dino-cache:${cacheId}`);
   for (const id of Object.keys(BASIC_SELLS)) if (!sells[id]) missing.push(`resource-sell:${id}`);
   for (const id of Object.keys(BOSS_TROPHY_SELLS)) if (!sells[id]) missing.push(`boss-sell:${id}`);
   for (const id of Object.keys(APEX_TRIBUTE_SELLS)) if (!sells[id]) missing.push(`apex-tribute-sell:${id}`);
@@ -37,6 +39,7 @@ function launchReadiness(profile) {
       sellItems: Object.keys(sells).length,
       requiredKits: 1 + Object.keys(KIT_PRICES).length,
       requiredResourceBuys: Object.keys(REBUNDLED_BUYS).length,
+      requiredInShopCaches: CACHE_IDS.length,
       requiredResourceSells: Object.keys(BASIC_SELLS).length,
       requiredBossSells: Object.keys(BOSS_TROPHY_SELLS).length,
       requiredApexTributeSells: Object.keys(APEX_TRIBUTE_SELLS).length
